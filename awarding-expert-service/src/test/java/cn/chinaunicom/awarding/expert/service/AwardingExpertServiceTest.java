61c1d40f1d4f34923b725cb60995ade4a023145f
limeng32 <limeng32@live.cn>
2017-01-12 11:43:10 +0800

    增加了测试用例AwardingExpertServiceTest，测试可以正常调用其它模块的基础代码

---
+++ awarding-expert-service/src/test/java/cn/chinaunicom/awarding/expert/service/AwardingExpertServiceTest.java
@@ -0,0 +1,64 @@
+package cn.chinaunicom.awarding.expert.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.IfProfileValue;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+import cn.chinaunicom.awarding.expert.persist.Acception;
+import cn.chinaunicom.awarding.expert.persist.AcceptionService;
+import cn.chinaunicom.awarding.expert.persist.Expert;
+import cn.chinaunicom.awarding.expert.persist.ExpertService;
+import cn.chinaunicom.awarding.project.persist.ProjectService;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+import com.github.springtestdbunit.dataset.FlatXmlDataSetLoader;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:expert-service-test.xml",
+		"classpath:project-service.xml", "classpath:account-service.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		DirtiesContextTestExecutionListener.class,
+		DbUnitTestExecutionListener.class })
+@DbUnitConfiguration(dataSetLoader = FlatXmlDataSetLoader.class)
+public class AwardingExpertServiceTest {
+
+	@Autowired
+	private AcceptionService acceptionService;
+
+	@Autowired
+	private ProjectService projectService;
+
+	@Autowired
+	private ExpertService expertService;
+
+	@Test
+	@IfProfileValue(name = "VOLATILE", value = "true")
+	@DatabaseSetup(type = DatabaseOperation.CLEAN_INSERT, value = "/cn/chinaunicom/awarding/expert/service/awardingExpertServiceTest/daoTest.xml")
+	@ExpectedDatabase(assertionMode = DatabaseAssertionMode.NON_STRICT, value = "/cn/chinaunicom/awarding/expert/service/awardingExpertServiceTest/daoTest.result.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "/cn/chinaunicom/awarding/expert/service/awardingExpertServiceTest/daoTest.result.xml")
+	public void daoTest() {
+		Acception acception = acceptionService.select("a");
+		Assert.assertNotNull(acception.getProject());
+		Assert.assertEquals("old", acception.getProject().getName());
+
+		acception.getProject().setName("new");
+		projectService.update(acception.getProject());
+
+		Expert expert = expertService.select("e");
+		expertService.update(expert);
+	}
+
+}
