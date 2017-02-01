b6c7485b79e8df8946aa703c5fc64a07bd4ef828
limeng32 <limeng32@live.cn>
2017-02-01 19:34:40 +0800

    增加了展示已邀请专家列表的方法

---
+++ awarding-expert-persist/src/main/java/cn/chinaunicom/awarding/expert/condition/TaskExpertCondition.java
@@ -0,0 +1,36 @@
+package cn.chinaunicom.awarding.expert.condition;
+
+import limeng32.mybatis.mybatisPlugin.cachePlugin.Conditionable;
+import limeng32.mybatis.mybatisPlugin.cachePlugin.Limitable;
+import limeng32.mybatis.mybatisPlugin.cachePlugin.Sortable;
+import cn.chinaunicom.awarding.expert.persist.TaskExpert;
+
+public class TaskExpertCondition extends TaskExpert implements Conditionable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Limitable limiter;
+
+	private Sortable sorter;
+
+	@Override
+	public Limitable getLimiter() {
+		return limiter;
+	}
+
+	@Override
+	public Sortable getSorter() {
+		return sorter;
+	}
+
+	@Override
+	public void setLimiter(Limitable arg0) {
+		this.limiter = arg0;
+	}
+
+	@Override
+	public void setSorter(Sortable arg0) {
+		this.sorter = arg0;
+	}
+
+}
--- awarding-expert-web/src/main/java/cn/chinaunicom/awarding/expert/web/ExpertCommonController.java
+++ awarding-expert-web/src/main/java/cn/chinaunicom/awarding/expert/web/ExpertCommonController.java
@@ -1,6 +1,7 @@
 package cn.chinaunicom.awarding.expert.web;
 
 import java.util.Collection;
+import java.util.LinkedHashSet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import cn.chinaunicom.awarding.expert.condition.ExpertCondition;
+import cn.chinaunicom.awarding.expert.condition.TaskExpertCondition;
 import cn.chinaunicom.awarding.expert.persist.Expert;
 import cn.chinaunicom.awarding.expert.persist.ExpertService;
 import cn.chinaunicom.awarding.expert.persist.TaskExpert;
@@ -69,6 +71,7 @@ public class ExpertCommonController {
 		tc.setStatus(TaskStatus.ongoing);
 		Collection<Task> taskC = taskService.selectAll(tc);
 		Task[] tasks = taskC.toArray(new Task[taskC.size()]);
+
 		ExpertCondition ec = new ExpertCondition();
 		ec.setLimiter(new PageParam(1, 10));
 		Collection<Expert> expertC = expertService.selectAll(ec);
@@ -78,10 +81,35 @@ public class ExpertCommonController {
 			taskExpertService.loadExpert(e, tec);
 		}
 		Page<Expert> page = new Page<>(expertC, ec.getLimiter());
+
 		Object[] ret = new Object[2];
 		ret[0] = tasks[0];
 		ret[1] = page;
 		callback.setData(ret);
+		return UNIQUE_VIEW_NAME;
+	}
+
+	@RequestMapping(method = { RequestMethod.POST }, value = "/expert/initExpertInvite2")
+	public String initExpertInvite2(HttpServletRequest request, ModelMap mm) {
+		Callback callback = new Callback();
+		mm.addAttribute("_content", callback);
+		TaskCondition tc = new TaskCondition();
+		tc.setLimiter((new PageParam(1, 1)));
+		tc.setSorter(new SortParam(new Order("begin",
+				Conditionable.Sequence.desc)));
+		tc.setStatus(TaskStatus.ongoing);
+		TaskExpertCondition tec2 = new TaskExpertCondition();
+		tec2.setTask(tc);
+		tec2.setLimiter(new PageParam(1, 10));
+		Collection<TaskExpert> taskExpertC = taskExpertService.selectAll(tec2);
+
+		Collection<Expert> expertC = new LinkedHashSet<>();
+		for (TaskExpert taskExpert : taskExpertC) {
+			expertC.add(taskExpert.getExpert());
+		}
+		Page<Expert> page = new Page<>(expertC, tec2.getLimiter());
+
+		callback.setData(page);
 		return UNIQUE_VIEW_NAME;
 	}
 
